475c5cde2d6040ae965ac028e5a9211681e59203
Burak Aydin <aslan.burak_@hotmail.com>
2025-03-18 00:21:58 +0300

    Front-end swiftUI project started

---
+++ Front-End/SmartShop/SmartShop/SmartShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartShopApp.swift
+//  SmartShop
+//
+//  Created by Burak Aydın on 18.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct SmartShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Front-End/SmartShop/SmartShopTests/SmartShopTests.swift
@@ -0,0 +1,17 @@
+//
+//  SmartShopTests.swift
+//  SmartShopTests
+//
+//  Created by Burak Aydın on 18.03.2025.
+//
+
+import Testing
+@testable import SmartShop
+
+struct SmartShopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
