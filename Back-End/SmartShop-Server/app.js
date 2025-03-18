fb42f8254e9a40808b1f419acdb4914d67392d0f
Burak Aydin <aslan.burak_@hotmail.com>
2025-03-18 07:07:13 +0300

    User Registration created in the backend

--- Back-End/SmartShop-Server/app.js
+++ Back-End/SmartShop-Server/app.js
@@ -1,42 +1,21 @@
 const express = require('express')
+const models = require('./models')
 const app = express()    // create an express app
 
-app.use(express.json())  // enable parsing of JSON data
+// JSON parser
+app.use(express.json())
 
-const movies = [
-    { title: 'Jaws', genre: 'action' },
-    { title: 'Avatar', genre: 'fiction' },
-    { title: 'Brazil', genre: 'kids' }
-]
+app.post('/register', (req, res) => {
+    const { username, password } = req.body
 
-app.get('/', (req, res) => {
-    res.send('ROOT')
+    //create a new user
+    const newUser = models.User.create({
+        username: username,
+        password: password
+    })
+
+    res.status(201).json({ succes: true })
 })
-
-app.get('/hello', (req, res) => {
-    res.json({ message: 'hello world' })
-})
-
-app.get('/movies', (req, res) => {
-    res.json(movies)
-})
-
-app.post('/movies', (req, res) => {
-    const { title, genre } = req.body
-    res.send('OK')
-})
-
-app.get('/movies/:genre', (req, res) => {
-    const genre = req.params.genre
-    res.json(movies.filter(movie => movie.genre.toLowerCase() == genre.toLowerCase()))
-})
-
-app.get('/movies/:genre/year/:year', (req, res) => {
-    const genre = req.params.genre
-    const year = parseInt(req.params.year)
-    res.send(`you selected ${genre} and the year is ${year}`)
-})
-
 
 //start the server
 app.listen(8080, () => {
