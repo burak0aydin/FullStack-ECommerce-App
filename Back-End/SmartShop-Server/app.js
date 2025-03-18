e194408d1b76ad9b77f413641921278c4e2bfbad
Burak Aydin <aslan.burak_@hotmail.com>
2025-03-18 23:41:12 +0300

    Catching Duplicate Username

--- Back-End/SmartShop-Server/app.js
+++ Back-End/SmartShop-Server/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const models = require('./models')
+const { Op } = require('sequelize')
 const { body, validationResult } = require('express-validator')
 const app = express()    // create an express app
 
@@ -7,27 +8,42 @@ const app = express()    // create an express app
 app.use(express.json())
 
 const registerValidator = [
-    body('username','username cannot be empty!').not().isEmpty(),
-    body('password','password cannot be empty!').not().isEmpty()
+    body('username', 'username cannot be empty!').not().isEmpty(),
+    body('password', 'password cannot be empty!').not().isEmpty()
 ]
 
-app.post('/register', registerValidator, (req, res) => {
-    
+app.post('/register', registerValidator, async (req, res) => {
+
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
         const msg = errors.array().map(error => error.msg).join('')
         return res.status(422).json({ success: false, message: msg })
     }
 
-    const { username, password } = req.body
+    try {
+        const { username, password } = req.body
 
-    //create a new user
-    const newUser = models.User.create({
-        username: username,
-        password: password
-    })
+        const existingUser = await models.User.findOne({
+            where: {
+                username: { [Op.iLike]: username }
+            }
+        })
 
-    res.status(201).json({ succes: true })
+        if (existingUser) {
+            return res.json({ message: 'Username taken!', success: false })
+        }
+
+        //create a new user
+        const newUser = models.User.create({
+            username: username,
+            password: password
+        })
+
+        res.status(201).json({ succes: true })
+    }
+    catch (error) {
+        res.status(500).json({ message: 'Internal server error', success: false })
+    }
 })
 
 //start the server
