a1202cbefcda11e0c45acf64db3309e05e3a2109
Burak Aydin <aslan.burak_@hotmail.com>
2025-03-22 02:59:11 +0300

    Implementing Authentication Controller - Back-End

--- Back-End/SmartShop-Server/app.js
+++ Back-End/SmartShop-Server/app.js
@@ -1,12 +1,7 @@
 const express = require('express')
-const models = require('./models')
-const { Op } = require('sequelize');
-const bcrypt = require('bcryptjs')
 const cors = require('cors')
 
 const authRoutes = require('./routes/auth')
-//const { body, validationResult } = require('express-validator');
-
 
 const app = express()
 
@@ -17,51 +12,6 @@ app.use(cors())
 app.use(express.json())
 // register our routers
 app.use('/api/auth', authRoutes)
-/*
-
-const registerValidator = [
-    body('username', 'username cannot be empty!').not().isEmpty(),
-    body('password', 'password cannot be empty.').not().isEmpty()
-]
-
-app.post('/api/auth/register', registerValidator, async (req, res) => {
-
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-        const msg = errors.array().map(error => error.msg).join('')
-        return res.status(422).json({ success: false, message: msg })
-    }
-
-    try {
-        const { username, password } = req.body
-
-        const existingUser = await models.User.findOne({
-            where: {
-                username: { [Op.iLike]: username }
-            }
-        })
-
-        if (existingUser) {
-            return res.json({ message: 'Username taken!', success: false })
-        }
-
-        // create a password hash 
-        const salt = await bcrypt.genSalt(10)
-        const hash = await bcrypt.hash(password, salt)
-
-        // create a new user 
-        const _ = models.User.create({
-            username: username,
-            password: hash
-        })
-
-        res.status(201).json({ success: true })
-    } catch (error) {
-        res.status(500).json({ message: 'Internal server error.', success: false })
-    }
-
-})
-    */
 
 // start the server 
 app.listen(8080, () => {
