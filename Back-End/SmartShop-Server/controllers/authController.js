464170e7b98e356099dfca5b32102615ebaec7e1
Burak Aydin <aslan.burak_@hotmail.com>
2025-03-22 03:46:36 +0300

    Authenticating the User and Returning the Token - Back-End

--- Back-End/SmartShop-Server/controllers/authController.js
+++ Back-End/SmartShop-Server/controllers/authController.js
@@ -1,7 +1,55 @@
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 const models = require('../models')
 const { Op } = require('sequelize')
 const { validationResult } = require('express-validator')
+
+exports.login = async (req, res) => {
+
+    try {
+
+    //validate the request
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        const msg = errors.array().map(error => error.msg).join('')
+        return res.status(422).json({ success: false, message: msg })
+    }
+
+    const { username, password } = req.body
+
+    //Check if the user exists
+    const existingUser = await models.User.findOne({
+        where: {
+            username: { [Op.iLike]: username }
+        }
+    })
+
+    //If the user does not exist
+    if (!existingUser) {
+        return res.status(401).json({ message: 'Invalid username or password.', success: false })
+    }
+
+    //Check if the password is correct
+    const isPasswordValid = await bcrypt.compare(password, existingUser.password)
+    if (!isPasswordValid) {
+        return res.status(401).json({ message: 'Invalid username or password.', success: false })
+    }
+    //If the password is correct, create a token
+    const token = jwt.sign({ userId: existingUser.id }, 'SECRETKEY', {
+        expiresIn: '1h'
+    })
+    
+    //Return the token and user information
+    return res.status(200).json({ userId: existingUser.id, username: existingUser.username, token, success: true })
+
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({ message: 'Internal server error.', success: false })
+    }
+
+}
+
+
 
 exports.register = async (req, res) => { 
 
--- Back-End/SmartShop-Server/routes/auth.js
+++ Back-End/SmartShop-Server/routes/auth.js
@@ -3,12 +3,17 @@ const router = express.Router()
 const { body }  = require('express-validator')
 const authController = require('../controllers/authController')
 
-
 const registerValidator = [
     body('username', 'username cannot be empty!').not().isEmpty(),
     body('password', 'password cannot be empty.').not().isEmpty()
 ]
 
+const loginValidator = [
+    body('username', 'username cannot be empty!').not().isEmpty(),
+    body('password', 'password cannot be empty.').not().isEmpty()
+]
+
 router.post('/register', registerValidator, authController.register)
+router.post('/login', registerValidator, authController.login)
 
 module.exports = router
