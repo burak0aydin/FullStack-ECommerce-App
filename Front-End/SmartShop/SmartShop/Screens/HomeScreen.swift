d8c0086a13c5e135fbfbf450ab12b1063a034575
Burak Aydin <aslan.burak_@hotmail.com>
2025-03-24 01:07:43 +0300

    Setting Up Tab Views - Front-End

---
+++ Front-End/SmartShop/SmartShop/Screens/HomeScreen.swift
@@ -0,0 +1,68 @@
+//
+//  HomeScreen.swift
+//  SmartShop
+//
+//  Created by Burak Aydın on 24.03.2025.
+//
+
+import SwiftUI
+
+enum AppScreen: Hashable, Identifiable, CaseIterable {
+    case home
+    case myProducts
+    case cart
+    case profile
+    
+    var id: AppScreen { self }
+}
+
+extension AppScreen {
+    
+    @ViewBuilder
+    var label: some View {
+        switch self {
+            case .home:
+                Label("Home", systemImage: "heart")
+            case .myProducts:
+                Label("My Products", systemImage: "star")
+            case .cart:
+                Label("Cart", systemImage: "cart")
+            case .profile:
+                Label("Profile", systemImage: "person.fill")
+        }
+    }
+    
+    @ViewBuilder
+    var destination: some View {
+        switch self {
+            case .home:
+                Text("Home")
+            case .myProducts:
+                Text("My Products")
+            case .cart:
+                Text("Cart")
+            case .profile:
+                Text("Profile")
+        }
+    }
+    
+}
+
+struct HomeScreen: View {
+    
+    @State var selection: AppScreen?
+    
+    var body: some View {
+        TabView(selection: $selection) {
+            ForEach(AppScreen.allCases) { screen in
+                screen.destination
+                    .tag(screen as AppScreen?)
+                    .tabItem { screen.label }
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeScreen()
+}
