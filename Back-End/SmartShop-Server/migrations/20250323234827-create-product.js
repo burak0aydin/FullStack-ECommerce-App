4b353921a98c6a41041d455af6ed25f8e5ba679c
Burak Aydin <aslan.burak_@hotmail.com>
2025-03-24 02:59:19 +0300

    Creating Product Table Using Sequelize CLI Tool and Migration - Back-End

---
+++ Back-End/SmartShop-Server/migrations/20250323234827-create-product.js
@@ -0,0 +1,46 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      price: {
+        type: Sequelize.FLOAT
+      },
+      photo_url: {
+        type: Sequelize.STRING
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users', // References the Users table
+          key: 'id'       // Points to the id column in the Users table
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Products');
+  }
+};
