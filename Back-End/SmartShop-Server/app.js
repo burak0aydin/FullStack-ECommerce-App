e673200526af2c5e1503fae37d3afaa89f656744
Burak Aydin <aslan.burak_@hotmail.com>
2025-03-29 08:03:57 +0300

    Creating Update User Info Route on Server - Back-End

--- Back-End/SmartShop-Server/app.js
+++ Back-End/SmartShop-Server/app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const authRoutes = require('./routes/auth')
 const productRoutes = require('./routes/product')
 const cartRoutes = require('./routes/cart') 
+const userRoutes = require('./routes/user')
 const authenticate = require('./middlewares/authMiddleware')
 
 const app = express()
@@ -23,6 +24,9 @@ app.use('/api/products', productRoutes)
 
 // cart routes 
 app.use('/api/cart', authenticate, cartRoutes)
+
+// user routes 
+app.use('/api/user', userRoutes)
 
 // start the server 
 app.listen(8080, () => {
